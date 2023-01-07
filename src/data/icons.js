f8f7322b1066993ba1f21f076bb8b4e070e847b2
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2023-01-07 11:44:49 -0300

    Update icons.js

--- src/data/icons.js
+++ src/data/icons.js
@@ -3,7 +3,7 @@ import IconPlay from '../assets/images/icon-play.svg';
 import IconSelfCare from '../assets/images/icon-self-care.svg';
 import IconSocial from '../assets/images/icon-social.svg';
 import IconStudy from '../assets/images/icon-study.svg';
-import IconExercise from '../assets/images/icon-Exercise.svg';
+import IconExercise from '../assets/images/icon-exercise.svg';
 import IconEllipsis from '../assets/images/icon-ellipsis.svg';
 
 export const icons = {
